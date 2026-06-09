4f0dfaa2c1d9c49fe0eb1e765ff1414fbebeb7dd
LauraML07 <laauraa.ml07@gmail.com>
2026-06-09 18:57:33 +0200

    Añadida la conexión con la hoja de estilos global CSS

--- src/main/java/org/example/proyectointerfaces/HelloApplication.java
+++ src/main/java/org/example/proyectointerfaces/HelloApplication.java
@@ -12,6 +12,8 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        String css = this.getClass().getResource("/css/style.css").toExternalForm();
+        scene.getStylesheets().add(css);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
